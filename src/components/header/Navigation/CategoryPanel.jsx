31ce489cda0c864435d4b57af0c2f5388ce040b0
codeTukai <tukaighosh753@gmail.com>
2025-05-17 21:18:14 +0530

    set

--- src/components/header/Navigation/CategoryPanel.jsx
+++ src/components/header/Navigation/CategoryPanel.jsx
@@ -14,7 +14,6 @@ const CategoryPanel = (props) => {
   const toggleDrawer = (newOpen) => () => {
     props.setIsOpenCatPanel(newOpen);
   };
-  
 
   const openSubmenu = (index) => {
     setSubmenuIndex((prevIndex) => (prevIndex === index ? null : index));
@@ -27,8 +26,10 @@ const CategoryPanel = (props) => {
     <Box sx={{ width: 250 }} role="presentation" className="categoryPanel">
       <h3 className="p-2 text-[16px] font-[500] flex items-center justify-between">
         Shop BY Category
-        <IoCloseSharp onClick={() => toggleDrawer(false)()} className="cursor-pointer text-[20px]" />
-
+        <IoCloseSharp
+          onClick={() => toggleDrawer(false)()}
+          className="cursor-pointer text-[20px]"
+        />
       </h3>
 
       <div className="scroll">
@@ -41,22 +42,17 @@ const CategoryPanel = (props) => {
               </Button>
             </Link>
 
-            {
-              submenuIndex === 0 ? (
-                <FaRegMinusSquare
+            {submenuIndex === 0 ? (
+              <FaRegMinusSquare
                 className="absolute top-[10px] right-[15px] cursor-pointer"
                 onClick={() => openSubmenu(0)}
               />
-
-              ):(
-
-                <FaRegSquarePlus
-              className="absolute top-[10px] right-[15px] cursor-pointer"
-              onClick={() => openSubmenu(0)}
-            />
-              )
-
-            }
+            ) : (
+              <FaRegSquarePlus
+                className="absolute top-[10px] right-[15px] cursor-pointer"
+                onClick={() => openSubmenu(0)}
+              />
+            )}
 
             {/* First-level submenu */}
             {submenuIndex === 0 && (
@@ -68,22 +64,17 @@ const CategoryPanel = (props) => {
                     </Button>
                   </Link>
 
-                  {
-              innerSubmenuIndex === 0 ? (
-                <FaRegMinusSquare
-                className="absolute top-[10px] right-[15px] cursor-pointer"
-                onClick={() => openInnerSubmenu(0)}
-              />
-
-              ):(
-
-                <FaRegSquarePlus
-              className="absolute top-[10px] right-[15px] cursor-pointer"
-              onClick={() => openInnerSubmenu(0)}
-            />
-              )
-
-              }
+                  {innerSubmenuIndex === 0 ? (
+                    <FaRegMinusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  ) : (
+                    <FaRegSquarePlus
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  )}
 
                   {innerSubmenuIndex === 0 && (
                     <ul className="submenu absolute top-[100%] left-0 w-full pl-3 bg-white z-10">
@@ -111,72 +102,69 @@ const CategoryPanel = (props) => {
             )}
           </li>
 
-
-
-
-
-
           <li className="list-none flex items-center relative">
-  <Link to="/" className="w-full">
-    <Button className="w-full text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
-      Outwear
-    </Button>
-  </Link>
+            <Link to="/" className="w-full">
+              <Button className="w-full text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Outwear
+              </Button>
+            </Link>
 
-  {submenuIndex === 1 ? (
-    <FaRegMinusSquare
-      className="absolute top-[10px] right-[15px] cursor-pointer"
-      onClick={() => openSubmenu(1)}
-    />
-  ) : (
-    <FaRegSquarePlus
-      className="absolute top-[10px] right-[15px] cursor-pointer"
-      onClick={() => openSubmenu(1)}
-    />
-  )}
-
-  {submenuIndex === 1 && (
-    <ul className="submenu absolute top-[100%] left-0 w-full pl-3 bg-white z-10">
-      <li className="list-item relative">
-        <Link to="/" className="w-full">
-          <Button className="w-full text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
-            Jackets
-          </Button>
-        </Link>
-
-        {innerSubmenuIndex === 1 ? (
-          <FaRegMinusSquare
-            className="absolute top-[10px] right-[15px] cursor-pointer"
-            onClick={() => openInnerSubmenu(1)}
-          />
-        ) : (
-          <FaRegSquarePlus
-            className="absolute top-[10px] right-[15px] cursor-pointer"
-            onClick={() => openInnerSubmenu(1)}
-          />
-        )}
-
-        {innerSubmenuIndex === 1 && (
-          <ul className="submenu absolute top-[100%] left-0 w-full pl-3 bg-white z-10">
-            {["Denim Jacket", "Blazer", "Raincoat", "Leather Coat"].map(
-              (item, idx) => (
-                <li key={idx} className="list-item relative mb-1">
-                  <Link
-                    to="/"
-                    className="link w-full text-left !px-3 transition text-[14px]"
-                  >
-                    {item}
-                  </Link>
-                </li>
-              )
+            {submenuIndex === 1 ? (
+              <FaRegMinusSquare
+                className="absolute top-[10px] right-[15px] cursor-pointer"
+                onClick={() => openSubmenu(1)}
+              />
+            ) : (
+              <FaRegSquarePlus
+                className="absolute top-[10px] right-[15px] cursor-pointer"
+                onClick={() => openSubmenu(1)}
+              />
             )}
-          </ul>
-        )}
-      </li>
-    </ul>
-  )}
-</li>
 
+            {submenuIndex === 1 && (
+              <ul className="submenu absolute top-[100%] left-0 w-full pl-3 bg-white z-10">
+                <li className="list-item relative">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Jackets
+                    </Button>
+                  </Link>
+
+                  {innerSubmenuIndex === 1 ? (
+                    <FaRegMinusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  ) : (
+                    <FaRegSquarePlus
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  )}
+
+                  {innerSubmenuIndex === 1 && (
+                    <ul className="submenu absolute top-[100%] left-0 w-full pl-3 bg-white z-10">
+                      {[
+                        "Denim Jacket",
+                        "Blazer",
+                        "Raincoat",
+                        "Leather Coat",
+                      ].map((item, idx) => (
+                        <li key={idx} className="list-item relative mb-1">
+                          <Link
+                            to="/"
+                            className="link w-full text-left !px-3 transition text-[14px]"
+                          >
+                            {item}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            )}
+          </li>
         </ul>
       </div>
     </Box>
