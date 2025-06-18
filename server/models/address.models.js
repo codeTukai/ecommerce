ab4f93529690e7d62911ee617dfc9d0d986368fc
codeTukai <tukaighosh753@gmail.com>
2025-06-18 13:51:46 +0530

    update

---
+++ server/models/address.models.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const addressSchema = mongoose.Schema ({
+    address_line :{
+        type: String,
+        default : ""
+    },
+   city :{
+        type: String,
+        default : ""
+    },
+    state :{
+        type: String,
+        default : ""
+    },
+    Pincode :{
+        type: String
+       
+    },
+    country :{
+        type: String
+        
+    },
+    mobile :{
+        type: Number,
+        default : null
+    },
+    status :{
+        type: Boolean,
+        default : true
+    },
+    userId :{
+        type: mongoose.Schema.ObjectId,
+        default : ""
+    }
+  
+},{
+    timestamps : true
+}
+
+
+)
+
+const AddressModel = mongoose.model('address', addressSchema)
+
+export default AddressModel;
